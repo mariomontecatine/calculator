ece04518ac631162b5d338db6de6283032973a45
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-09-22 17:55:51 +0200

    Basic structure and first functions

---
+++ main.js
@@ -0,0 +1,26 @@
+let operator;
+let number1;
+let number2;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(type, num1, num2) {
+
+}
+
+
+
