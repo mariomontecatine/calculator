5254cd125c4e8eb13b5d248f1cd35dfd28690938
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-09-22 19:00:19 +0200

    Functional buttons and display initially done

--- main.js
+++ main.js
@@ -22,5 +22,33 @@ function operate(type, num1, num2) {
 
 }
 
+const buttonValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "+", "-", "*", "/", "CLEAR"];
+const buttonsContainer = document.getElementById("buttonsContainer");
+const display = document.getElementById("display");
+
+buttonValues.forEach(val => {
+    const btn = document.createElement("button");
+    btn.textContent = val;
+    btn.setAttribute("id", `btn${val}`);
+    buttonsContainer.appendChild(btn);
+});
+
+buttonsContainer.addEventListener("click", function(e) {
+    if (e.target && e.target.nodeName == "BUTTON") {
+        let numPressed = e.target.id.replace("btn","");
+        displayNumbers(numPressed);
+    }
+});
+
+let displayCurrentValue = 0;
+function displayNumbers(num) {
+    display.textContent = num;
+    displayCurrentValue = num;
+}
+
+
+
+
+
 
 
