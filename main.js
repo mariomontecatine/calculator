fe54d485b03e89fb3a301ac14b5b431e4df92843
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-09-23 09:48:21 +0200

    Now, numbers concatenate each other in the display

--- main.js
+++ main.js
@@ -15,11 +15,23 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    
     return num1 / num2;
 }
 
 function operate(type, num1, num2) {
-
+    if (type === "+") {
+        displayNumbers(add(num1,num2));
+    }
+    else if (type === "-") {
+        displayNumbers(subtract(num1,num2));
+    }
+    else if (type === "*") {
+        displayNumbers(multiply(num1,num2));
+    }
+    else {
+        displayNumbers(divide(num1,num2));
+    }
 }
 
 const buttonValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "=", "+", "-", "*", "/", "CLEAR"];
@@ -33,10 +45,32 @@ buttonValues.forEach(val => {
     buttonsContainer.appendChild(btn);
 });
 
-buttonsContainer.addEventListener("click", function(e) {
+// Event delegation
+buttonsContainer.addEventListener("click", function (e) {
     if (e.target && e.target.nodeName == "BUTTON") {
-        let numPressed = e.target.id.replace("btn","");
-        displayNumbers(numPressed);
+        let numPressed = e.target.id.replace("btn", "");
+        // si es numero permitir acoplarlos (en plan si pones 1 y luego 2: 12)
+        // true si numPressed NO ES UN NUMERO
+        // cuando se le de a "=" se llama a operate
+        if (isNaN(numPressed) && numPressed === "=") {
+            operate(operator, number1, number2);
+        }
+        // cuando se presione un operator se almacena el numero anterior y se lee otro
+        else if (isNaN(numPressed)) {
+            number1 = number2;
+            operator = numPressed;
+        }
+        else {
+            if (number2 === undefined) {
+                number2 = numPressed;
+            }
+            else {
+                number2 += numPressed;
+            }
+            
+        }
+        // se actualiza el display (y tambien intermediamente con cada numero) 
+        displayNumbers(number2);
     }
 });
 
@@ -45,6 +79,8 @@ function displayNumbers(num) {
     display.textContent = num;
     displayCurrentValue = num;
 }
+
+
 
 
 
